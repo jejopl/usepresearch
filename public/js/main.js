0d615ded3060348a1e87d54fe08274eeead98f44
unknown <jejopl@o2.pl>
2019-05-30 13:24:33 +0200

    fixed first page

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('#searchForm')
+const input = document.querySelector('#searchInput')
+const tryLink = document.querySelector('#tryLink')
+const copyLink = document.querySelector('#copyLink')
+const copyDiv = document.querySelector('#copyDiv')
+
+
+copyLink.addEventListener('mousedown', e => {
+  e.preventDefault()
+  let query = encodeURIComponent(input.value.trim())
+  query = query.replace(/%20/g, '+');
+  copyToClipboard(`https://${window.location.host}/p/${query}`)
+  copyDiv.innerHTML = 'COPIED!'
+  setTimeout(() => {
+    copyDiv.innerHTML = 'Copy link'
+  }, 500)
+})
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+})
+
+input.addEventListener('keyup', e => {
+  let query = encodeURIComponent(input.value.trim())
+  tryLink.href = `/p/${query}`
+})
+
+function copyToClipboard(text) {
+  const textarea = document.createElement("textarea");
+  document.body.appendChild(textarea);
+  textarea.value = text;
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+}
